c1809547e7afd4d990233aa27d5884a3888c878e
Phan Minh <zad942000@gmail.com>
2024-02-27 22:43:12 +0700

    build infrastructure

---
+++ Library/src/main/java/com/ecommerce/library/service/Impl/AdminServiceImpl.java
@@ -0,0 +1,8 @@
+package com.ecommerce.library.service.Impl;
+
+import com.ecommerce.library.service.AdminService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdminServiceImpl implements AdminService {
+}
